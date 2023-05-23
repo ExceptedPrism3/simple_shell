dfe398e8e8c4483bc8d7f725a46725c13e70ce87
ExceptedPrism3 <ben.ayoube@outlook.com>
2023-05-23 16:42:59 -0700

    xD

---
+++ betty.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
+#define PROMPT "#cisfun$ "
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *buffer;
+	size_t bufsize = BUFFER_SIZE;
+
+	buffer = malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
+
+	while (1)
+	{
+		printf(PROMPT);
+		if (getline(&buffer, &bufsize, stdin) == -1)
+		{
+			printf("\n");
+			break;
+		}
+
+		/* Remove newline character */
+		buffer[strcspn(buffer, "\n")] = '\0';
+
+		/* Fork a child process to execute the command */
+		pid_t pid = fork();
+		if (pid < 0)
+		{
+			perror("Unable to fork");
+			exit(1);
+		}
+		else if (pid == 0)
+		{
+			/* Child process */
+			if (execlp(buffer, buffer, NULL) == -1)
+			{
+				printf("%s: No such file or directory\n", buffer);
+				exit(1);
+			}
+		}
+		else
+		{
+			/* Parent process */
+			wait(NULL);
+		}
+	}
+
+	free(buffer);
+	return 0;
+}
+
