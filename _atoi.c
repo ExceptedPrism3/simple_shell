620d2050c6b94c45da8e0e02c04121cf37d0ef59
Prism <68008341+ExceptedPrism3@users.noreply.github.com>
2023-05-24 01:52:29 +0100

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,38 +1,51 @@
-/**
- * main - entry point for application
- * @ac: argument count
- * @av: argument vector
- * Return: 0 on success
- */
-int main(int ac, char **av)
-{
-	config build;
-	(void)ac;
-	signal(SIGINT, handle_sigint);
-	config_init(&build);
-	build.shell_name = av[0];
-	shell(&build);
-	return (0);
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+typedef struct {
+    // Define necessary members for the config struct
+    int env;
+    char *env_list;
+    char **args;
+    char *buffer;
+    char *path;
+    char *full_path;
+    int count_line;
+    char *shell_name;
+    int error_status;
+} config;
+
+void handle_sigint(int sig) {
+    // Handle SIGINT signal
+    printf("SIGINT signal received\n");
+    // Implement the necessary logic here
 }
 
-/**
- * config_init - initialize member values for config struct
- * @build: input build
- *
- * Description: Accepts structured input commnad, and finds the right
- * configuration to execute from the built-in configurations.
- * Return: build with initialized members
- */
-config *config_init(config *build)
-{
-	build->env = generateLinkedList(environ);
-	build->env_list = NULL;
-	build->args = NULL;
-	build->buffer = NULL;
-	build->path = _getenv("PATH", environ);
-	build->full_path = NULL;
-	build->count_line = 0;
-	build->shell_name = NULL;
-	build->error_status = 0;
-	return (build);
+config *config_init(config *build) {
+    // Implement the initialization of config struct members
+    build->env = 0;
+    build->env_list = NULL;
+    build->args = NULL;
+    build->buffer = NULL;
+    build->path = NULL;
+    build->full_path = NULL;
+    build->count_line = 0;
+    build->shell_name = NULL;
+    build->error_status = 0;
+    return build;
+}
+
+void shell(config *build) {
+    // Implement the shell logic here
+    printf("Shell logic\n");
+    // Implement the necessary logic here
+}
+
+int main(int ac, char **av) {
+    config build;
+    signal(SIGINT, handle_sigint);
+    config_init(&build);
+    build.shell_name = av[0];
+    shell(&build);
+    return 0;
 }
