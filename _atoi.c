530b809bef97f6e00e575a334938db15f6f1f3ff
Prism <68008341+ExceptedPrism3@users.noreply.github.com>
2023-05-24 01:47:51 +0100

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,29 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 
-typedef struct config {
-    /* Members of config struct */
-    // ...
+typedef struct {
+    // Add necessary members here
 } config;
 
-config *config_init(config *build);
-void handle_sigint(int signum);
-void shell(config *build);
-char *_getenv(const char *name, char **env);
-void generateLinkedList(char **env);
+void handle_sigint(int sig) {
+    // Handle SIGINT signal
+}
 
-int main(int ac, char **av)
-{
-    config build = {0};
+void shell(config *build) {
+    // Implement shell logic here
+}
+
+config *config_init(config *build) {
+    build->env = generateLinkedList(environ);
+    build->env_list = NULL;
+    build->args = NULL;
+    build->buffer = NULL;
+    build->path = _getenv("PATH", environ);
+    build->full_path = NULL;
+    build->count_line = 0;
+    build->shell_name = NULL;
+    build->error_status = 0;
+    return build;
+}
+
+int main(int ac, char **av) {
+    config build;
     signal(SIGINT, handle_sigint);
     config_init(&build);
     build.shell_name = av[0];
     shell(&build);
     return 0;
-}
-
-config *config_init(config *build)
-{
-    build->env = generateLinkedList(environ);
-    // Initialize other members
-    return build;
 }
