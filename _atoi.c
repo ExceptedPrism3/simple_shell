1c023d95e386e1d196ff8997d794bf8211c9976e
Prism <68008341+ExceptedPrism3@users.noreply.github.com>
2023-05-24 01:48:21 +0100

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,37 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-
-typedef struct {
-    // Add necessary members here
-} config;
-
-void handle_sigint(int sig) {
-    // Handle SIGINT signal
+/**
+ * main - entry point for application
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	config build;
+	(void)ac;
+	signal(SIGINT, handle_sigint);
+	config_init(&build);
+	build.shell_name = av[0];
+	shell(&build);
+	return (0);
 }
 
-void shell(config *build) {
-    // Implement shell logic here
-}
-
-config *config_init(config *build) {
-    build->env = generateLinkedList(environ);
-    build->env_list = NULL;
-    build->args = NULL;
-    build->buffer = NULL;
-    build->path = _getenv("PATH", environ);
-    build->full_path = NULL;
-    build->count_line = 0;
-    build->shell_name = NULL;
-    build->error_status = 0;
-    return build;
-}
-
-int main(int ac, char **av) {
-    config build;
-    signal(SIGINT, handle_sigint);
-    config_init(&build);
-    build.shell_name = av[0];
-    shell(&build);
-    return 0;
+/**
+ * config_init - initialize member values for config struct
+ * @build: input build
+ *
+ * Description: Accepts structured input commnad, and finds the right
+ * configuration to execute from the built-in configurations.
+ * Return: build with initialized members
+ */
+config *config_init(config *build)
+{
+	build->env = generateLinkedList(environ);
+	build->env_list = NULL;
+	build->args = NULL;
+	build->buffer = NULL;
+	build->path = _getenv("PATH", environ);
+	build->full_path = NULL;
+	build->count_line = 0;
+	build->shell_name = NULL;
+	build->error_status = 0;
+	return (build);
 }
