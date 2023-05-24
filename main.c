2cf2b2921d42b05c7fe0ae038b196dfe3f14ea3b
ExceptedPrism3 <ben.ayoube@outlook.com>
2023-05-23 18:29:19 -0700

    xD

--- main.c
+++ main.c
@@ -1,44 +1,23 @@
-#include "shell.h"
+#include "holberton.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
- *
- * Return: 0 on success, 1 on error
- */
-int main(int ac, char **av)
+ * main - shell proyect
+ * @ac: count arguments
+ * @av: store arguments
+ * @env: variable envirionment system
+ * Return: estatus_exit value;
+ **/
+
+int main(int ac, char *av[], char **env)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+	int count_exe = 1, status_exit = 0;
+	(void)ac;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
-
-	if (ac == 2)
+	if (ac > 1)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
+		non_interact(av);
+		return (0);
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+	status_exit = interactive(av, count_exe, env);
+	return (status_exit);
 }
